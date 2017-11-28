70de8d0dc1d5f991823411031da46898c777fa0d
Othmar Lechner <lindrope@hotmail.com>
2017-11-28 19:05:40 +0100

    Move styles of the content script into own folder to provide a similar folder structure (->../img/...)

--- copyStylesheet.js
+++ copyStylesheet.js
@@ -1,4 +1,4 @@
 const fs = require('fs');
 
 const copyFile = (from, to) => fs.writeFile(to, fs.readFileSync(from).toString());
-copyFile('./src/css/style.css', './build/contentScripts/style.css');
+copyFile('./src/css/style.css', './build/contentScripts/css/style.css');
