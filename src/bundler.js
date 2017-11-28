ee7478dfcefa0136d8803682d7c8967dc8863eb0
Othmar Lechner <lindrope@hotmail.com>
2017-11-28 19:01:49 +0100

    Load css via extension and don't include it into the script to prevent overriding styles of the original page (e.g. #main)

--- src/bundler.js
+++ src/bundler.js
@@ -1,5 +1,4 @@
 import mainHTML from './html/index.html';
-import mainCSS from './css/style.css';
 import executeScript from './js/script_with_export';
 
 const addCSS = function(style) {
@@ -12,5 +11,4 @@ const addHTML = function(html) {
 };
 
 addHTML(mainHTML);
-addCSS(mainCSS);
 executeScript();
