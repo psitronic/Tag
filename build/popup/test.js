36910ca2ddf6554392b9d3a699184ee2ce9c3b87
okram ovic <okram@protonmail.ch>
2017-11-28 14:48:14 +0100

    just test - adding a comment line in test.js

--- build/popup/test.js
+++ build/popup/test.js
@@ -1,10 +1,6 @@
-<<<<<<< HEAD
 //alert("injecting");
 
 document.body.innerHTML = "<h1>a Tag!</h1>" + document.body.innerHTML;
-||||||| merged common ancestors
-=======
-//alert("injecting");
 
-document.body.innerHTML = "<h1>a Tag!</h1>" + document.body.innerHTML;
->>>>>>> a3f8a063836f8c31c2345c17225bdc153c0607dc
+
+// test test test for github branch
