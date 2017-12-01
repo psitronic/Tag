9781fb7bb6664baa1071ab3588b17469144c0e23
marko <okram@protonmail.ch>
2017-12-01 18:04:49 +0100

    fixing indefinite reloading possibility

--- build/contentScripts/accountScript.js
+++ build/contentScripts/accountScript.js
@@ -4,10 +4,22 @@ $(function(){
 
   chrome.runtime.sendMessage({getLoginData: true}, function(response){
 
-          console.log("msg sent, recieved this:\n", response)
+          //console.log("msg sent, recieved this:\n", response)
+          //let origin = "https://stickertags2.glitch.me"
 
-          $.post("/manageAccount", {_id: response.session.userId})
+          if (location.href === "https://stickertags2.glitch.me/manageAccount")
+          
+             $.post("/manageAccount", {_id: response.session.userId}, function(data, status, xhr){
+
+                    if (!data) console.error("no URL subpath received")
+                    else {
+                      console.log("resp", data)
+
+                      location.href =  location.origin + data
+
+                      console.log("location", location)
+                    }
+              })
 
   })
 })
-
